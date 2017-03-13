0981530c226eff92bf4ae484074fc9e4ca617d0d
Kiran <kiranreddyamk@gmail.com>
2017-03-13 17:29:52 -0400

    Initial commit for starter boot and no arch types selected

---
+++ src/main/java/com/example/kiran/KiranBootPrjApplication.java
@@ -0,0 +1,12 @@
+package com.example.kiran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiranBootPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiranBootPrjApplication.class, args);
+	}
+}
