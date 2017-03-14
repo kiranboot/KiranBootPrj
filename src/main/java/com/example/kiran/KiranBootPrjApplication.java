e463a09bf228a2325f86626fc96f55ed7f0734f2
Kiran <kiranreddyamk@gmail.com>
2017-03-14 10:40:37 -0400

    Running the Boot application on Tomcat

--- src/main/java/com/example/kiran/KiranBootPrjApplication.java
+++ src/main/java/com/example/kiran/KiranBootPrjApplication.java
@@ -2,11 +2,26 @@ package com.example.kiran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KiranBootPrjApplication {
+public class KiranBootPrjApplication extends SpringBootServletInitializer {
 
+	/**
+	 *  Used when run as jar (packaging in the pom.xml)
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(KiranBootPrjApplication.class, args);
 	}
+
+	/**
+	 *  Used when run as war (packaging in the pom.xml)
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(KiranBootPrjApplication.class);
+	}
+	
+	
 }
